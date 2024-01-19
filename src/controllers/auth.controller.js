952c690bb63380ed632dd19629832dc195086ce8
Ronald Sancho <ronalsancho@gmail.com>
2024-01-18 18:37:43 -0600

    Encrypt password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,18 @@
 import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 
 export const register = async (req, res) => {
 
     const {email, password, userName} = req.body
 
     try{
+
+        const passwordHash = await bcrypt.hash(password, 10)
+
         const newUser = new User({
             userName,
             email,
-            password
+            password: passwordHash,
         });
     
         const userSaved = await newUser.save();
