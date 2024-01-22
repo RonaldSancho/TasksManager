580d73bba7d22e36bbcc7710a3946a55b391b05e
Ronald Sancho <ronalsancho@gmail.com>
2024-01-21 21:05:59 -0600

    Create createTask

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -5,7 +5,17 @@ export const getTasks = async (req, res) => {
     res.json(tasksFound);
 }
 
-export const getTask = async (req, res) => {}
+export const getTask = async (req, res) => {
+    const {title, description, date} = req.body;
+
+    const newTask = new Task({
+        title,
+        description,
+        date,
+    });
+    const savedTask = await newTask.save();
+    res.json(savedTask);
+}
 
 export const createTask = async (req, res) => {}
 
