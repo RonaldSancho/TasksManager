9ef578fca04592874f0a17107dc2dda3f11232ae
Ronald Sancho <ronalsancho@gmail.com>
2024-01-30 18:46:53 -0600

    Create registerSchema

---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,17 @@
+import zod from 'zod'
+
+export const resgiterSchema = zod.object({
+    userName: zod.string({
+        required_error: 'Username is required'
+    }),
+    email: zod.string({
+        required_error: 'Email is required'
+    }).email({
+        message: 'Invalid email'
+    }),
+    password: zod.string({
+        required_error: 'Password is required'
+    }).min(6,{
+        message: 'Password must be least 6 character'
+    }),
+})
