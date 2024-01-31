dd68d2286e4a40d123a538b6eac7f7db89366af0
Ronald Sancho <ronalsancho@gmail.com>
2024-01-30 19:28:00 -0600

    Edit task.schema

--- src/schemas/task.schema.js
+++ src/schemas/task.schema.js
@@ -4,8 +4,8 @@ export const createTaskSchema = zod.object({
     title: zod.string({
         required_error: 'Title is required'
     }),
-    description: z.string({
+    description: zod.string({
         required_error: 'Description must be a string'
-    }).optional(),
+    }),
     date: zod.string().datetime().optional(),
 })
