9616faf45cdf48062affe8dff49b14a22792d9ea
Ronald Sancho <ronalsancho@gmail.com>
2024-01-30 18:50:57 -0600

    Create loginSchema

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -15,3 +15,16 @@ export const resgiterSchema = zod.object({
         message: 'Password must be least 6 character'
     }),
 })
+
+export const loginSchema = zod.object({
+    email: zod.string({
+        required_error: 'Email is required'
+    }).email({
+        message: 'Invalid email'
+    }),
+    password: zod.string({
+        required_error: 'Password is required'
+    }).min(6,{
+        message: 'Password must be least 6 character'
+    }),
+})
